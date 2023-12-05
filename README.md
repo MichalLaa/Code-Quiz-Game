d1369dcc7194e170a7efd7d27ed3a5ee7aa9952d
Michal Lasa <lasa.michal@gmail.com>
2023-12-05 22:23:22 +0000

    i've added js code and i finish readme file

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # Code-Quiz-Game
+
+# Installation
+Download the file from github, then add it to a folder where you can open it by using VS (visual sutdio code),
+
+
+# General Info
+I created a quiz that contains several questions about coding, points are added after each question and information is displayed whether the person playing answered the question correctly or incorrectly.
+At the end of the quiz, a table appears that saves all the results and names of the players, where there is also an option to return to the quiz and play again or delete all results.
+This quiz was intended to demonstrate my coding skills in HTML, CSS and JavaScript.
+
+# Quiz
+
+Start Quiz.
+
+Here what you see when you start of the quiz.
+
+![Start](/assets/images/Start%20of%20the%20quiz.png)
+
+# Questions
+
+Here is the one of the questions where you need to choose one question.
+
+![Questions](/assets/images/question.png)
+
+# Correct Anwser 
+
+You will see a correct text at the bottom if you answerd correct.
+
+![Correct answer](/assets/images/Correct%20anwser.png)
+
+
+# Wrong Anwser
+
+Here is what you will see when you anwser wrong.
+
+
+![Wrong answer](/assets/images/Wrong%20answer.png)
+
+
+# Highscores 
+
+Thats is Socre page which you will see after finish all the questions or after running of time, you have 60 sec to anwser all quiestions after that time you will see last page with the score table.
+
+![last page](/assets/images/score.png)
+
+
+# License
+ 
+ MIT License.
---
+++ assets/js/questions.js
@@ -0,0 +1,85 @@
+// list of all questions, choices, and answers
+var questions = [
+  
+    {
+      title: "What the name of HTML stnad for  ?",
+      choices: [
+        "Hypertext Markup Language",
+        "Hypertext Make Language",
+        "Hightext Markup Language",
+        "Hightext Make Language"
+      ],
+      answer: "Hypertext Markup Language"
+    },
+    {
+      title: "What the name of CSS stnad for ?",
+      choices: [
+        "Coding Style Sheets",
+        "Cascading Super Spredsheet", 
+        "Cascading Style Sheets", 
+        "Coding Spred Sheets"
+      ],
+      answer: "Cascading Style Sheets"
+    },
+    {
+      title: "What is GitHub used for?",
+      choices: [
+        "Platform for hosting code that allows for version control and collaboration",
+        "Platform to play online games",
+        "Social media website",
+        "All the above"
+      ],
+      answer: "Platform for hosting code that allows for version control and collaboration"
+    },
+    {
+      title:
+        "What is a Date Structure?",
+      choices: [
+        "Specific way of organizing data in a specialized format on a computer",
+        "Specific way of organizing data in a specialized format on your Phone",
+        "Specific way of organizing data on the paper",
+        "Date where you store your images"
+      ],
+      answer: "Specific way of organizing data in a specialized format on a computer"
+    },
+    {
+      title: "How to write comments in HTML?",
+      choices: [
+        "<!-- .. -->",
+        "// ... //",
+        "!-- .. --!",
+        "none of the above"
+      ],
+      answer: "<!-- .. -->"
+    },
+    {
+      title: "What s the use of comment in code ?",
+      choices: [
+        "Placing Human Readable Descriptions inside of computer programs detailing what the Code is doing",
+       "To write code easliy", 
+       "To end code", 
+       "To let know computer what to do"
+      ],
+      answer: "Placing Human Readable Descriptions inside of computer programs detailing what the Code is doing"
+    },
+    {
+      title: "Which one are coding program",
+      choices: [
+        "visual studio code",
+        "Adobe",
+        "Git Bash",
+        "Bootstrap"
+      ],
+      answer: "visual studio code"
+    },
+    {
+      title: "HTML it use to create _______.",
+      choices: ["Webpages", "Mobile App", "Computer game", "Write CV"],
+      answer: "Webpages"
+    },
+    {
+      title: "Which are not programing language?",
+      choices: ["JavaScript", "HTML", "CSS", "IG"],
+      answer: "IG"
+    },
+  ];
